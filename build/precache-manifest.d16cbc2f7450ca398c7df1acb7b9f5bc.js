c66212fd02f23209a94ca2b1edd12101702ff69c
unknown <yt.ozgur@gmail.com>
2020-05-19 23:03:23 +0300

    Initial commit

---
+++ build/precache-manifest.d16cbc2f7450ca398c7df1acb7b9f5bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a119c4a54829ad23e7f1a7fcd2b265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e475efd17633949acdb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "614c29f75c0619efca64",
+    "url": "/static/js/2.051a2960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.051a2960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e475efd17633949acdb",
+    "url": "/static/js/main.18d7829f.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
