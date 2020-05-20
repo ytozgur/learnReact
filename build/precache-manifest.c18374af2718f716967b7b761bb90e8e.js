d050c43507043abdda0a84ba662b2fbac8885fae
Özgür <yt.ozgur@gmail.com>
2020-05-20 20:21:03 +0300

    adding files for github pages

---
+++ build/precache-manifest.c18374af2718f716967b7b761bb90e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452d10a94a85ae5d9e698772e8b0688e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9e97a5757d373e8a321a",
+    "url": "/monsters-rolodex/static/css/main.b85ea100.chunk.css"
+  },
+  {
+    "revision": "0ee236b4b2b6cc1ac15d",
+    "url": "/monsters-rolodex/static/js/2.b7351722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b7351722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e97a5757d373e8a321a",
+    "url": "/monsters-rolodex/static/js/main.f2ca07ab.chunk.js"
+  },
+  {
+    "revision": "fff867ebe5f155b62abe",
+    "url": "/monsters-rolodex/static/js/runtime-main.7e0dcc88.js"
+  }
+]);
